e2c79812e559febd997518e484487ccb5a37219b
Wavednight <wavednight@gmail.com>
2023-12-10 18:57:01 +0700

    Задача1

--- Homework001/Program.cs
+++ Homework001/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        PrintNumbers(m, n);
+
+        Console.ReadLine();
+    }
+
+    static void PrintNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.Write(m + " ");
+            PrintNumbers(m + 1, n);
+        }
+    }
+}
