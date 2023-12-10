911995d310ec0df114a0b7c0cf3abb058d12ba67
Wavednight <wavednight@gmail.com>
2023-12-10 19:08:34 +0700

    Задача3

--- Homework003/Program.cs
+++ Homework003/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        PrintArrayReversed(array, array.Length - 1);
+
+        Console.ReadLine();
+    }
+
+    static void PrintArrayReversed(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(array[index]);
+            PrintArrayReversed(array, index - 1);
+        }
+    }
+}
